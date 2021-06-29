16d2d245a16578b347cba5d392e41479ea218ade
anastasia2503 <86655139+anastasia2503@users.noreply.github.com>
2021-06-29 17:11:55 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,15 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+parts = text.split(' ')
+print(parts)
+x = 0
+res = 0
+while x < len(parts):
+    if len(parts[x]) > 5:
+        res += 1
+    else:
+        pass
+    x += 1
+print(res)
