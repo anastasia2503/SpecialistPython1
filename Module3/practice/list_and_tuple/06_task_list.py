a78b4918936ec15d654a789f93a604d210bc0684
anastasia2503 <86655139+anastasia2503@users.noreply.github.com>
2021-06-30 15:04:10 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,14 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+a = []
+x = first_number
+while x <= second_number:
+    a.append(x)
+    x += 1
+
+n = []
+for el in a:
+    if el % 3 == 0:
+        n.append(el)
+print(n)
