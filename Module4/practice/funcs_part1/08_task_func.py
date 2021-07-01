a7342a4c0fee615cd8bcba847ad728ab265e1c96
anastasia2503 <86655139+anastasia2503@users.noreply.github.com>
2021-07-01 13:24:22 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,16 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+
+n = int(input())
+
+def fibonachi(n):
+    i = 0
+    fib1 = 1
+    fib2 = 1
+    while i < n - 2:
+        fib = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib
+        i += 1
+    return fib2
+
+print(fibonachi(n))
