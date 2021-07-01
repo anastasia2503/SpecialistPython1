f00bf9635f92a264bfbe6b745bb2b81caff498a1
anastasia2503 <86655139+anastasia2503@users.noreply.github.com>
2021-07-01 12:48:32 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -2,11 +2,15 @@
 # Напишите функцию, проверябщую можно ли построить треугольник, соединив данные точки отрезками
 
 def can_triangle(p1, p2, p3):
-    # TODO: your code here
-    pass
-
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    lenght12 = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    lenght13 = ((x3 - x1)**2 + (y3 - y1)**2)**0.5
+    lenght23 = ((x3 - x2)**2 + (y3 - y2)**2)**0.5
+    if lenght13 + lenght23 < lenght12 and lenght12 + lenght13 < lenght23 and lenght23 + lenght12 < lenght13:
+        return lenght13 + lenght23 < lenght12
+    return lenght13 + lenght23 < lenght12
 
 # Пример вызова функции
-can_triangle((10, 12), (14, 18), (12, 12))
-
-# Не забудьте протестировать вашу функцию
+print(can_triangle((10, 12), (14, 18), (12, 12)))
